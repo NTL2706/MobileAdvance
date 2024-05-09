458a38a8167940b6a0784e00caf91e5b950c52a6
Hnng2011 <luphihung111@gmail.com>
2024-05-10 01:15:20 +0700

    update project

--- lib/feature/projects/provider/project_provider.dart
+++ lib/feature/projects/provider/project_provider.dart
@@ -13,17 +13,24 @@ class ProjectProvider extends ChangeNotifier {
       HttpResponse<List<Map<String, dynamic>>>.unknown();
   List<Map<String, dynamic>>? favouriteProjectList;
   List<Project> _projects = [];
-  List<Project> _filteredProjects = [];
-  int _selectedProjectLength = 0;
+  final List<Project> _filteredProjects = [];
+
+  int _selectedProjectLength = 4;
   final TextEditingController _numberOfStudentsController =
+      TextEditingController();
+  final TextEditingController _numberOfProposalsController =
       TextEditingController();
   final TextEditingController _searchController = TextEditingController();
 
   List<Project> get projects => _filteredProjects;
+
   int get selectedProjectLength => _selectedProjectLength;
   TextEditingController get numberOfStudentsController =>
       _numberOfStudentsController;
+  TextEditingController get numberOfProposalsController =>
+      _numberOfProposalsController;
   TextEditingController get searchController => _searchController;
+
   bool? hasMore = true;
 
   Future<void> toggleFavoriteStatus(
@@ -58,10 +65,12 @@ class ProjectProvider extends ChangeNotifier {
 
   void updateSearch() {
     _filteredProjects.clear();
+    hasMore = true;
     notifyListeners();
   }
 
   void filterSearch() {
+    hasMore = true;
     _filteredProjects.clear();
     notifyListeners();
   }
@@ -72,32 +81,57 @@ class ProjectProvider extends ChangeNotifier {
   }
 
   void resetFilter() {
+    hasMore = true;
+    _selectedProjectLength = 4;
+    _numberOfStudentsController.clear();
+    _numberOfProposalsController.clear();
     notifyListeners();
   }
 
-  Future<HttpResponse<List<Map<String, dynamic>>>> getAllProjectForStudent(
+  Future<HttpResponse<List<Map<String, dynamic>>>?> getAllProjectForStudent(
       {required String token,
       required int studentId,
       int? page,
       int? perPage,
       String? title,
-      int? projectScopeFlag,
-      int? numberOfStudents,
-      int? proposalsLessThan}) async {
+      String? projectScopeFlag,
+      String? numberOfStudents,
+      String? proposalsLessThan}) async {
+    if (hasMore == false) {
+      return null;
+    }
     try {
       responseHttp = HttpResponse<List<Map<String, dynamic>>>.unknown();
-      hasMore = true;
+
+      final Map<String, String> queryParameters = {};
+
+      if (title != '') {
+        queryParameters['title'] = title!;
+      }
+      if (numberOfStudents != '') {
+        queryParameters['numberOfStudents'] = numberOfStudents!;
+      }
+      if (projectScopeFlag != '4') {
+        queryParameters['projectScopeFlag'] = projectScopeFlag!;
+      }
+      if (proposalsLessThan != '') {
+        queryParameters['proposalsLessThan'] = proposalsLessThan!;
+      }
+      if (title == '' &&
+          numberOfStudents == '' &&
+          projectScopeFlag == '4' &&
+          proposalsLessThan == '') {
+        queryParameters['page'] = page.toString();
+        queryParameters['perPage'] = perPage.toString();
+      }
+
       final uri = Uri(
         scheme: 'https',
         host: env.apiURL?.replaceAll("https://", "").replaceAll("/", ""),
         path: 'api/project',
-        queryParameters: {
-          "page":page.toString(),
-          "perPage":perPage.toString(),
-          "title": title,
-        }
+        queryParameters: queryParameters,
       );
-      print(uri);
+
       final rs = await http.get(
           headers: {HttpHeaders.authorizationHeader: 'Bearer $token'}, uri);
       final favoriteProjectResponse = await http.get(
@@ -107,26 +141,27 @@ class ProjectProvider extends ChangeNotifier {
       final bodyFavourite = json.decode(favoriteProjectResponse.body);
       final body = json.decode(rs.body);
 
-     if (rs.statusCode >= 400) {
-        throw Exception(body);
+      if (rs.statusCode >= 400) {
+        throw Exception(body['errorDetails']);
       }
 
-     
       final result = List<Map<String, dynamic>>.from(body['result']);
       _projects = result.map((e) {
         return Project.fromJson(e);
       }).toList();
+
+      if (_projects.length < perPage!) {
+        hasMore = false;
+      }
 
       _filteredProjects.addAll(_projects);
 
       final favouriteProject =
           List<Map<String, dynamic>>.from(bodyFavourite['result']);
       favouriteProjectList = favouriteProject;
- 
-      
-      
+
       responseHttp.updateResponse({"result": result, "status": rs.statusCode});
-      
+
       return responseHttp as HttpResponse<List<Map<String, dynamic>>>;
     } on Exception catch (e) {
       print(e);
@@ -138,7 +173,6 @@ class ProjectProvider extends ChangeNotifier {
   Future<void> checkApply(
       {required String token, required int studentId}) async {
     try {
-     
       final rs = await http.get(
           headers: {HttpHeaders.authorizationHeader: "Bearer $token"},
           Uri.parse("${env.apiURL}api/proposal/student/$studentId"));
@@ -157,8 +191,7 @@ class ProjectProvider extends ChangeNotifier {
           return proposal['projectId'] == element.id;
         });
       });
-
-    }on Exception catch (e) {
+    } on Exception catch (e) {
       print(e);
     }
   }
--- lib/feature/projects/views/filter_modal.dart
+++ lib/feature/projects/views/filter_modal.dart
@@ -4,102 +4,131 @@ import 'package:provider/provider.dart';
 import '../provider/project_provider.dart';
 
 class FilterModal extends StatelessWidget {
-// Giá trị mặc định là chuỗi rỗng
+  // Giá trị mặc định là chuỗi rỗng
   FilterModal({super.key});
 
   @override
   Widget build(BuildContext context) {
     final projectProvider = Provider.of<ProjectProvider>(context);
-    late ScrollController _scrollController;
-    _scrollController = ScrollController();
+    final ScrollController scrollController = ScrollController();
 
     return SingleChildScrollView(
-        controller: _scrollController,
-        child: Container(
-          padding: EdgeInsets.only(
-              top: 20,
-              left: 20,
-              right: 20,
-              bottom: MediaQuery.of(context).viewInsets.bottom + 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text('Filter by Project Length:'),
-              Column(
-                children: [
-                  RadioListTile<int>(
-                    title: Text('All'),
-                    value: 0,
-                    groupValue: projectProvider.selectedProjectLength,
-                    onChanged: (value) {
-                      projectProvider.setSelectedProjectLength(value!);
-                    },
-                  ),
-                  RadioListTile<int>(
-                    title: Text('1 to 3 months'),
-                    value: 1,
-                    groupValue: projectProvider.selectedProjectLength,
-                    onChanged: (value) {
-                      projectProvider.setSelectedProjectLength(value!);
-                    },
-                  ),
-                  RadioListTile<int>(
-                    title: Text('4 to 6 months'),
-                    value: 2,
-                    groupValue: projectProvider.selectedProjectLength,
-                    onChanged: (value) {
-                      projectProvider.setSelectedProjectLength(value!);
-                    },
-                  ),
-                  RadioListTile<int>(
-                    title: Text('More than 6 months'),
-                    value: 3,
-                    groupValue: projectProvider.selectedProjectLength,
-                    onChanged: (value) {
-                      projectProvider.setSelectedProjectLength(value!);
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text('Enter Number of Students:'),
-              TextField(
-                onTap: () => {
-                  SystemChannels.textInput
-                      .invokeMethod('TextInput.show')
-                      .then((_) {
-                    // Khi bàn phím xuất hiện, cuộn xuống cuối của SingleChildScrollView
-                    _scrollController.animateTo(
-                      _scrollController.position.maxScrollExtent,
-                      duration: Duration(milliseconds: 300),
-                      curve: Curves.ease,
-                    );
-                  })
-                },
-                keyboardType: TextInputType.number,
-                controller: projectProvider.numberOfStudentsController,
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      projectProvider.filterSearch();
-                    },
-                    child: Text('Apply Filter'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      projectProvider.resetFilter();
-                    },
-                    child: Text('Clear Filter'),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+      controller: scrollController,
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 20,
+          left: 20,
+          right: 20,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Filter by Project Length:'),
+            Column(
+              children: [
+                RadioListTile<int>(
+                  title: Text('All'),
+                  value: 4,
+                  groupValue: projectProvider.selectedProjectLength,
+                  onChanged: (value) {
+                    projectProvider.setSelectedProjectLength(value!);
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('<1 months'),
+                  value: 0,
+                  groupValue: projectProvider.selectedProjectLength,
+                  onChanged: (value) {
+                    projectProvider.setSelectedProjectLength(value!);
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('1 to 3 months'),
+                  value: 1,
+                  groupValue: projectProvider.selectedProjectLength,
+                  onChanged: (value) {
+                    projectProvider.setSelectedProjectLength(value!);
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('4 to 6 months'),
+                  value: 2,
+                  groupValue: projectProvider.selectedProjectLength,
+                  onChanged: (value) {
+                    projectProvider.setSelectedProjectLength(value!);
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('More than 6 months'),
+                  value: 3,
+                  groupValue: projectProvider.selectedProjectLength,
+                  onChanged: (value) {
+                    projectProvider.setSelectedProjectLength(value!);
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text('Enter Number of Students:'),
+            TextField(
+              onTap: () => {
+                SystemChannels.textInput
+                    .invokeMethod('TextInput.show')
+                    .then((_) {
+                  // Khi bàn phím xuất hiện, cuộn xuống cuối của SingleChildScrollView
+                  scrollController.animateTo(
+                    scrollController.position.maxScrollExtent,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
+                })
+              },
+              keyboardType: TextInputType.number,
+              controller: projectProvider.numberOfStudentsController,
+            ),
+            SizedBox(height: 20),
+            Text('Enter Number of Proposals:'),
+            TextField(
+              onTap: () => {
+                SystemChannels.textInput
+                    .invokeMethod('TextInput.show')
+                    .then((_) {
+                  // Khi bàn phím xuất hiện, cuộn xuống cuối của SingleChildScrollView
+                  scrollController.animateTo(
+                    scrollController.position.maxScrollExtent,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
+                })
+              },
+              keyboardType: TextInputType.number,
+              controller: projectProvider.numberOfProposalsController,
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    projectProvider.filterSearch();
+                    Navigator.pop(context); // Close the modal
+                  },
+                  child: Text('Apply Filter'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    projectProvider.resetFilter();
+                    Navigator.pop(context); // Close the modal
+                  },
+                  child: Text('Clear Filter'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
